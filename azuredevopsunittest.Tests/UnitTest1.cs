658a0e0da005d9d5afc74a46fc0b7ab37bea11f4
mounagam <62638836+mounagam@users.noreply.github.com>
2020-04-09 01:46:17 -0400

    Add files via upload

---
+++ azuredevopsunittest.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using azuredevopsunittest;
+
+namespace azuredevopsunittest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void Addition()
+        {
+            int a = 5;
+            int b = 4;
+            int c;
+            c = service.add(a, b);
+            Assert.AreEqual("9", c.ToString(), "Addition method success");
+        }
+        [TestMethod]
+        public void Multiplication()
+        {
+            int a = 5;
+            int b = 4;
+            int c;
+            c = service.multi(a, b);
+            Assert.AreEqual("20", c.ToString(), "Multiplication method success");
+        }
+        [TestMethod]
+        public void Minus()
+        {
+            int a = 5;
+            int b = 4;
+            int c;
+            c = service.minus(a, b);
+            Assert.AreEqual("1", c.ToString(), "Minus method success");
+        }
+    }
+}
