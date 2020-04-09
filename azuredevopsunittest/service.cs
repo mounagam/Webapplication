67522688c6ed33a9a7ff03fd252424f5e0b31cfd
mounagam <62638836+mounagam@users.noreply.github.com>
2020-04-09 01:40:49 -0400

    Add files via upload

---
+++ azuredevopsunittest/service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace azuredevopsunittest
+{
+    public static class service
+    {
+        public static int add(int a, int b)
+        {
+
+            return Businesslayer.add(a, b);
+        }
+
+        public static int multi(int a, int b)
+        {
+
+            return Businesslayer.multi(a, b);
+        }
+        public static int minus(int a, int b)
+        {
+
+            return Businesslayer.minus(a, b);
+        }
+    }
+}
